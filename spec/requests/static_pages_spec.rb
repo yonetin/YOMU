2fa9b38c56e4ca84b964ec8e9f67251abe693da9
yonetin <yonekou1110@gmail.com>
2015-02-25 00:07:46 +0900

    Finish refact

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,28 +5,56 @@ describe "Static Pages" do
   let(:base_title) { "Ruby on Rails Tutorial Sample App | " }
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_content(heading) }
+    it { should have_title("#{base_title}#{page_title}") }
+  end
+
   describe "Home page" do
     before { visit root_path }
-    it { should have_content('Sample App') }
-    it { should have_title("#{base_title}Home") }
+    let(:heading) { 'Sample App' }
+    let(:page_title) { 'Home' }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "Help page" do
     before { visit help_path }
-    it { should have_content('Help') }
-    it { should have_title("#{base_title}Help") }
+    let(:heading) { 'Help' }
+    let(:page_title) { 'Help' }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before { visit about_path }
-    it { should have_content('About Us') }
-    it { should have_title("#{base_title}About Us") }
+    let(:heading) { 'About' }
+    let(:page_title) { 'About Us' }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
     before { visit contact_path }
-    it { should have_content('Contact') }
-    it { should have_title("#{base_title}Contact") }
+    let(:heading) { 'Contact' }
+    let(:page_title) { 'Contact' }
+
+    it_should_behave_like "all static pages"
   end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title("#{base_title}About Us")
+    click_link "Help"
+    expect(page).to have_title("#{base_title}Help")
+    click_link "Contact"
+    expect(page).to have_title("#{base_title}Contact")
+    # click_link "Sign up now!"
+    # expect(page).to have_title("#{base_title}Sign up")
+    click_link "sample app"
+    expect(page).to have_title("#{base_title}Home")
+ 
+  end 
 end
 
